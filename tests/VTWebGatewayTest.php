ebecf75111c27d81b85fb9285ff19ef0d45cf1f9
Andy Librian <andylibrian@gmail.com>
2015-02-18 13:57:10 +0700

    test vtweb gateway

---
+++ tests/VTWebGatewayTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Omnipay\Veritrans;
+
+class VTWebGatewayTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->gateway = new VTWebGateway();
+    }
+
+    public function testGetName()
+    {
+        $this->assertEquals('Veritrans VT-Web', $this->gateway->getName());
+    }
+
+    public function testGetDefaultParameters()
+    {
+        $this->assertEquals(
+            array(
+                'serverKey'   => '',
+                'environment' => 'production',
+            ),
+            $this->gateway->getDefaultParameters()
+        );
+    }
+
+    public function testSetGetServerKey()
+    {
+        $this->gateway->setServerKey('key');
+        $this->assertEquals('key', $this->gateway->getServerKey());
+    }
+
+    public function testSetGetEnvironment()
+    {
+        $this->gateway->setEnvironment('env');
+        $this->assertEquals('env', $this->gateway->getEnvironment());
+    }
+
+    public function testAuthorize()
+    {
+        $this->assertInstanceOf(
+            'Omnipay\Veritrans\Message\VTWeb\TransactionChargeRequest',
+            $this->gateway->authorize()
+        );
+    }
+}
