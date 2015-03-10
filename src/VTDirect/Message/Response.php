ddb65db807630478a00d5f7723b7523a2b6d762a
Andy Librian <andylibrian@gmail.com>
2015-03-10 09:25:23 +0700

    add vtdirect mandiri clickpay

---
+++ src/VTDirect/Message/Response.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Omnipay\Veritrans\VTDirect\Message;
+
+use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
+
+class Response extends AbstractResponse implements RedirectResponseInterface
+{
+    public function isSuccessful()
+    {
+        return in_array($this->getCode(), array('200'));
+    }
+
+    public function getCode()
+    {
+        return $this->data['status_code'];
+    }
+
+    public function getMessage()
+    {
+        return $this->data['status_message'];
+    }
+
+    public function isRedirect()
+    {
+        return !empty($this->data['redirect_url']);
+    }
+
+    public function getRedirectMethod()
+    {
+        return 'GET';
+    }
+
+    public function getRedirectUrl()
+    {
+        return $this->data['redirect_url'] ?: null;
+    }
+
+    public function getRedirectData()
+    {
+        return array();
+    }
+}
