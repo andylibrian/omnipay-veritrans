871e47cc7698bc0a1f2680dd6125645005c4f6c7
Thomas Faurbye Nielsen <Archanium@gmail.com>
2016-08-03 15:12:14 +0200

    Add item_details if there are any items defined on the product.

--- src/Message/VTWeb/TransactionChargeRequest.php
+++ src/Message/VTWeb/TransactionChargeRequest.php
@@ -2,6 +2,7 @@
 
 namespace Omnipay\Veritrans\Message\VTWeb;
 
+use Omnipay\Common\Item;
 use Omnipay\Veritrans\Message\AbstractRequest;
 
 class TransactionChargeRequest extends AbstractRequest
@@ -26,36 +27,49 @@ class TransactionChargeRequest extends AbstractRequest
             $amountInteger /= 100;
         }
 
-        $data = array(
-            'payment_type' => 'vtweb',
-            'vtweb' => $this->getVtwebConfiguration(),
+        $data  = array(
+            'payment_type'        => 'vtweb',
+            'vtweb'               => $this->getVtwebConfiguration(),
             'transaction_details' => array(
-                'order_id' => $this->getTransactionId(),
+                'order_id'     => $this->getTransactionId(),
                 'gross_amount' => $amountInteger,
             ),
-            'customer_details' => array(
-                'first_name' => $this->getCard()->getFirstName(),
-                'last_name' => $this->getCard()->getLastName(),
-                'email' => $this->getCard()->getEmail(),
-                'phone' => $this->getCard()->getBillingPhone(),
-                'billing_address' => array(
-                    'first_name' => $this->getCard()->getFirstName(),
-                    'last_name'  => $this->getCard()->getLastName(),
-                    'address' => $this->getCard()->getBillingAddress1(),
-                    'city' => $this->getCard()->getBillingCity(),
-                    'postal_code' => $this->getCard()->getBillingPostcode(),
-                    'phone' => $this->getCard()->getBillingPhone(),
+            'customer_details'    => array(
+                'first_name'       => $this->getCard()->getFirstName(),
+                'last_name'        => $this->getCard()->getLastName(),
+                'email'            => $this->getCard()->getEmail(),
+                'phone'            => $this->getCard()->getBillingPhone(),
+                'billing_address'  => array(
+                    'first_name'   => $this->getCard()->getFirstName(),
+                    'last_name'    => $this->getCard()->getLastName(),
+                    'address'      => $this->getCard()->getBillingAddress1(),
+                    'city'         => $this->getCard()->getBillingCity(),
+                    'postal_code'  => $this->getCard()->getBillingPostcode(),
+                    'phone'        => $this->getCard()->getBillingPhone(),
                     'country_code' => $this->getCard()->getBillingCountry(),
                 ),
                 'shipping_address' => array(
-                    'address' => $this->getCard()->getShippingAddress1(),
-                    'city' => $this->getCard()->getShippingCity(),
-                    'postal_code' => $this->getCard()->getShippingPostcode(),
-                    'phone' => $this->getCard()->getShippingPhone(),
+                    'address'      => $this->getCard()->getShippingAddress1(),
+                    'city'         => $this->getCard()->getShippingCity(),
+                    'postal_code'  => $this->getCard()->getShippingPostcode(),
+                    'phone'        => $this->getCard()->getShippingPhone(),
                     'country_code' => $this->getCard()->getShippingCountry(),
                 ),
             ),
         );
+        $items = $this->getItems();
+        if ($items) {
+            /** @var Item $item */
+            foreach ($items as $item) {
+                $data['item_details'][] = [
+                    'price'         => $item->getPrice(),
+                    'quantity'      => $item->getQuantity(),
+                    'id'            => substr($item->getName(), 0, min(strlen($item->getName()), 50)),
+                    'name'          => substr($item->getName(), 0, min(strlen($item->getName()), 50)),
+                    'merchant_name' => substr($item->getName(), 0, min(strlen($item->getName()), 50)),
+                ];
+            }
+        }
 
         $dataStr = json_encode($data);
 
@@ -64,6 +78,6 @@ class TransactionChargeRequest extends AbstractRequest
 
     public function getEndpoint()
     {
-        return $this->getBaseUrl().'/charge';
+        return $this->getBaseUrl() . '/charge';
     }
 }
--- tests/Message/VTWeb/TransactionChargeRequestTest.php
+++ tests/Message/VTWeb/TransactionChargeRequestTest.php
@@ -31,6 +31,29 @@ class TransactionChargeRequestTest extends TestCase
         $this->assertNotEmpty($this->request->getData());
     }
 
+    public function testGetDataWithItems()
+    {
+        $this->request->initialize(
+            array(
+                'amount'   => '12000.00',
+                'currency' => 'IDR',
+                'card'     => $this->getValidCard(),
+                'items'    => [['name' => 'Test', 'price' => '100.00', 'quantity' => 2,]]
+            )
+        );
+        $this->assertNotEmpty($this->request->getData());
+        $this->assertTrue(strpos($this->request->getData(), 'item_details') !== false);
+        $this->request->initialize(
+            array(
+                'amount'   => '12000.00',
+                'currency' => 'IDR',
+                'card'     => $this->getValidCard(),
+            )
+        );
+        $this->assertNotEmpty($this->request->getData());
+        $this->assertFalse(strpos($this->request->getData(), 'item_details'));
+    }
+
     public function testGetEndpoint()
     {
         $this->assertEquals($this->request->getBaseUrl() . '/charge', $this->request->getEndPoint());
